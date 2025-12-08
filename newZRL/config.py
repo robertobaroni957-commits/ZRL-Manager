c1d8aa7f552749e5be9960dc8c03110bef4b4c9c
robertobaroni957-commits <roberto.baroni957@gmail.com>
2025-12-08 17:36:29 +0100

    Fix missing closing parenthesis for SECRET_KEY

--- newZRL/config.py
+++ newZRL/config.py
@@ -6,4 +6,4 @@ class Config:
         "postgresql+psycopg://zrlmanager_db_user:teF9dx9z82SvMVHq6wT6MZPQ1c6nZmyQ@dpg-d4res6ngi27c73amg2e0-a.frankfurt-postgres.render.com/zrlmanager_db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key"
