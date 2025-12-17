0cc44e30274d311deec9a67d5d6c130db1f90dd6
robertobaroni957-commits <roberto.baroni957@gmail.com>
2025-12-17 17:43:15 +0100

    modificate file config per render

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Run database migrations
+echo "Running database migrations..."
+python run_migrations.py
+echo "Migrations complete."
+
+# Print a message and start the main application
+echo "Starting application server..."
+waitress-serve --host=0.0.0.0 --port=${PORT} run:app
